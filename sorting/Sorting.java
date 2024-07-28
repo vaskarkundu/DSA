423f55787edc4773a9aa211a6168da717a19a243
Vaskar Kundu <vaskarkundu18@gmail.com>
2024-07-28 22:05:23 +0600

    feat : implemented insertation sort algo

--- sorting/Sorting.java
+++ sorting/Sorting.java
@@ -16,7 +16,7 @@ public class Sorting {
         // bubble sort ==> time complexcity O(n^2)
         for(int i =0; i<arr.length - 1; i++){
             for(int j=0; j<arr.length - i - 1; j++){
-                if(arr[j]<arr[j+1]){
+                if(arr[j]>arr[j+1]){
                     int tem = arr[j];
                     arr[j]=arr[j+1];
                     arr[j+1]=tem;
@@ -26,7 +26,7 @@ public class Sorting {
 
         printArray(arr);
 
-        // selection sort
+        // selection sort ==> time complexcity O(n^2)
 
         for(int i = 0; i<arr.length - 1;i++){
             int smallest = i;
@@ -39,10 +39,24 @@ public class Sorting {
             int temp = arr[smallest];
             arr[smallest]=arr[i];
             arr[i]=temp;
-
-
         }
 
+        printArray(arr);
+
+        // Insertation sort
+
+        for(int i = 1; i<arr.length; i++){
+            int curr = arr[i];
+            int j = i - 1;
+            while (j >=0 && curr < arr[j]) {
+                arr[j+1]=arr[j];
+                j--;
+                
+            }
+
+            // placement
+            arr[j+1] = curr;
+        }
         printArray(arr);
 
         
