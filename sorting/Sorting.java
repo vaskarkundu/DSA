13f2ff4fe98136d0cd069b631b8eab87367e662d
Vaskar Kundu <vaskarkundu18@gmail.com>
2024-07-28 11:52:58 +0600

    feat : implemented bubble sort

--- sorting/Sorting.java
+++ sorting/Sorting.java
@@ -1,5 +1,32 @@
 package sorting;
 
 public class Sorting {
+
+    public static void printArray(int arr[]) {
+        for(int i=0; i<arr.length; i++) {
+            System.out.print(arr[i]+" ");
+        }
+        System.out.println();
+    }
+ 
+
+    public static void main(String[] args) {
+       
+        int arr[]={4,1,5,3,9,6};
+        // bubble sort
+        for(int i =0; i<arr.length - 1; i++){
+            for(int j=0; j<arr.length - i - 1; j++){
+                if(arr[j]<arr[j+1]){
+                    int tem = arr[j];
+                    arr[j]=arr[j+1];
+                    arr[j+1]=tem;
+                }
+            }
+        }
+
+        printArray(arr);
+
+        
+    }
     
 }
