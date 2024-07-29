1dbdf707c6977ea4a471fd3f189d94549f2753f3
Vaskar Kundu <vaskarkundu18@gmail.com>
2024-07-29 23:49:10 +0600

    feat : solution of a pblm in recursion

--- Recursion/Recursion.java
+++ Recursion/Recursion.java
@@ -1,10 +1,20 @@
 package Recursion;
 
 public class Recursion {
+   
 
+    // print 5 to 1;
+    public static void printNum(int n){
+        if(n==0){
+            return;
+        }
+        System.out.println(n);
+        printNum(n-1);
+    }
 
     public static void main(String[] args) {
         //recursion
+        printNum(5);
     }
     
 }
