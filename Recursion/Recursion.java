c614d72c542e0b65129ecc74b22c4428eba5f4ea
Vaskar Kundu <vaskarkundu18@gmail.com>
2024-07-30 00:17:47 +0600

    solution : sum of the first N nutural number

--- Recursion/Recursion.java
+++ Recursion/Recursion.java
@@ -5,16 +5,31 @@ public class Recursion {
 
     // print 5 to 1;
     public static void printNum(int n){
-        if(n==0){
+        if(n>5){
             return;
         }
         System.out.println(n);
-        printNum(n-1);
+        printNum(n+1);
+    }
+
+    // print sum of the first N nutural number
+
+    public static void printNaturalSum(int i,int n, int sum){
+        if(i==n){
+            sum +=i;
+            System.out.println(sum);
+            return;
+        }
+        sum +=i;
+        printNaturalSum(i+1, n, sum);
+       
+       
     }
 
     public static void main(String[] args) {
         //recursion
-        printNum(5);
+        // printNum(1);
+        printNaturalSum(1,5,0);
     }
     
 }
