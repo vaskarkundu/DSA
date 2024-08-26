a0f52827fad5b2ee256efd565c55708a4513849b
Vaskar Kundu <vaskarkundu18@gmail.com>
2024-08-26 20:11:43 +0600

    solution : added similar pair of string

--- String/String_question.java
+++ String/String_question.java
@@ -1,5 +1,8 @@
 package String;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class String_question {
 
     public static void reverseString(char[] s) {
@@ -13,11 +16,37 @@ public class String_question {
             right --;
         }   
     }
+    // similar pari of string;
+    public static int similarPairs(String[] words) {
+
+        
+        int count = 0;
+        for(int i =0; i<words.length;i++){
+            Set<Character> set1 = new HashSet<>();
+            for(char c : words[i].toCharArray()){
+                set1.add(c);
+            }
+
+            for(int j = i+1;j<words.length;j++){
+                Set<Character> set2 = new HashSet<>();
+                for(char c : words[j].toCharArray()){
+                    set2.add(c);
+                }
+                if(set1.equals(set2)){
+                    count++;
+                }
+            }
+
+        }
+        return count;
+        
+    }
 
     public static void main(String[] args) {
         char[] myCharArray = {'a', 'b', 'c', 'd', 'e'};
-        reverseString(myCharArray);
-        System.out.println('v');
+        String[] c = {"aba","aabb","abcd","bac","aabc"};
+        int x =similarPairs(c);
+        System.out.println(x);
     }
     
 }
