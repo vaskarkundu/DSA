c8100a25ad19dc5e6093df1b09fddb425e22955c
Vaskar Kundu <vaskarkundu18@gmail.com>
2024-09-01 13:04:19 +0600

    solution : all possible combination of string

--- Backtracking/Backtracking_question.java
+++ Backtracking/Backtracking_question.java
@@ -1,8 +1,26 @@
 package Backtracking;
 
 public class Backtracking_question {
+    // all possible combination of a string ==> O(n * n!)
+    public static void permutationOfString(String str,String perm, int n){
+
+        if(str.length() == 0){
+            System.out.println(perm);
+            return;
+        }
+        for(int i =0; i<str.length(); i++){
+            char curr = str.charAt(i);
+            String neString = str.substring(0, i) + str.substring(i+1);
+            
+            permutationOfString(neString, perm + curr, n+1);
+        }
+
+    }
 
     public static void main(String[] args) {
+
+        String x = "abc";
+        permutationOfString(x, "", 0);
         
     }
     
