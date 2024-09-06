335aef18b9178094fdb69202e5a0a8455dd06565
Vaskar Kundu <vaskarkundu18@gmail.com>
2024-09-07 00:12:15 +0600

    solution : duplicate number

--- sorting/Sorting_Question.java
+++ sorting/Sorting_Question.java
@@ -1,5 +1,6 @@
 package sorting;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -29,13 +30,66 @@ public class Sorting_Question {
         return res;
         
     }
+    //  slip overlap interval 
+    public static int[][] merge(int[][] intervals) {
+        int[][] ar = new int[2][2];
+
+        ArrayList<Integer> res = new ArrayList<>();
+        for (int i = 0; i < intervals.length; i++) {           
+            for (int j = 0; j < intervals[i].length; j++) {     
+                res.add(intervals[i][j]);
+            }
+            
+        }
+        int left = 0;
+        int right = 1;
+        ArrayList<Integer> arr = new ArrayList<>();
+        for(int i = 0; i<res.size() - 1; i++){
+            if(res.get(i) < res.get(i+1)){
+               arr.add(res.get(i));
+            }
+
+        }
+       
+      
+        System.out.println(arr);
+        System.out.println(res);
+
+        return intervals;
+        
+    }
+
+    public static boolean containsDuplicate(int[] nums) {
+
+        Map<Integer, Integer> numberMap = new HashMap<>();
+        boolean res = false;
+
+        for(int i = 0; i<nums.length; i++){
+            numberMap.put(nums[i], numberMap.getOrDefault(nums[i], 0)+ 1);
+        }
+        for (Map.Entry<Integer, Integer> entry : numberMap.entrySet()){
+            Integer value = entry.getValue();
+            if(value > 1){
+                res = true;
+
+            }
+            
+        }
+
+        return res;
+        
+    }
     
 
 
     public static void main(String[] args) {
-        int[] x = {3,2,3};
-        int y =majorityElement(x);
+        int[] x = {3,2,4,2};
+        boolean y = containsDuplicate(x);
         System.out.println(y);
+        // int y =majorityElement(x);
+        // System.out.println(y);
+        // int[][] x = {{1,2},{2,3}};
+        // int[][] y = merge(x); 
         
     }
 }
