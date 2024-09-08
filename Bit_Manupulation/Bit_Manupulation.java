b22d0341a6f3a4b2b6d76475b8b6051e7bc60e1f
Vaskar Kundu <vaskarkundu18@gmail.com>
2024-09-08 16:19:00 +0600

    solution : implemented clear bit

--- Bit_Manupulation/Bit_Manupulation.java
+++ Bit_Manupulation/Bit_Manupulation.java
@@ -28,15 +28,25 @@ public class Bit_Manupulation {
         System.out.println(newNum);
 
     }
-    // 0101 | 0010 => 0111
+
+    public static void clearBit(int n , int pos){
+        int bitMask = ~(1<<pos);
+
+        int newNumber = bitMask & n;
+        System.out.println(newNumber);
+
+    }
+    
+    // 0101 & 0100 ~ 1011 ==>  0001
 
     public static void main(String[] args) {
 
         // Get Bit operation
         int n = 5;
-        int pos = 1;
+        int pos = 2;
         // getBit(n, pos);
-        setBit(n, pos);
+        // setBit(n, pos);
+        clearBit(n, pos);
       
 
         // Set Bit operation
