0ed0f65e3b77a3142ae135a34c095f5c5b6c6b17
Vaskar Kundu <vaskarkundu18@gmail.com>
2024-09-17 12:57:33 +0600

    solution : isUgly number

--- Number/Numeric_Question.java
+++ Number/Numeric_Question.java
@@ -1,15 +1,41 @@
 package Number;
 
+import java.util.ArrayList;
+
 public class Numeric_Question {
+
+    public static boolean recur(int n,int pr){
+        int[] primefactor ={2,3,5};
+        if(primefactor.length -1 < pr)return false;
+        if(n == 1)return true;
+       if(n % primefactor[pr] != 0){
+        recur(n, pr+1);
+       }else{
+        int m = n / primefactor[pr];
+        // System.out.println(primefactor[pr]);
+        recur(m, pr+1);
+       }
+
+       return false;
+       
+    }
 
 
     public static boolean isUgly(int n) {
+          
+           if (n <= 0) return false;
+           if (n == 1) return true;
+           if (n % 2 == 0) return isUgly(n / 2);
+           if (n % 3 == 0) return isUgly(n / 3);
+           if (n % 5 == 0) return isUgly(n / 5);
+          
+           return false;
 
-        return true;
-        
     }
 
     public static void main(String[] args) {
+        boolean x = isUgly(6);
+        System.out.println(x);
         
     }
     
