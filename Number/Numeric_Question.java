92b0f63e8dfb4346bda1dcc9a3d13733f46b1e95
Vaskar Kundu <vaskarkundu18@gmail.com>
2024-09-17 14:51:36 +0600

    solution : reverse a number

--- Number/Numeric_Question.java
+++ Number/Numeric_Question.java
@@ -33,8 +33,35 @@ public class Numeric_Question {
 
     }
 
+    public static int reversHelper(int x){
+        return reverse(x, 0);
+
+    }
+
+    public static int reverse(int x,int res) {
+        
+       if(x == 0){
+        return res;
+       }
+      
+       int reminder = x % 10;
+        
+         if (res > Integer.MAX_VALUE / 10 || (res == Integer.MAX_VALUE / 10 && reminder > 7)) {
+            return 0; 
+        }
+        if (res < Integer.MIN_VALUE / 10 || (res == Integer.MIN_VALUE / 10 && reminder < -8)) {
+            return 0; 
+        }
+
+
+
+
+       res = res * 10 + reminder;
+       return reverse(x/10,res); 
+    }
+
     public static void main(String[] args) {
-        boolean x = isUgly(6);
+        int x = reversHelper(1534236469);
         System.out.println(x);
         
     }
