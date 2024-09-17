82ec5abd310cce4d6aa889a1d61d399caa744882
Vaskar Kundu <vaskarkundu18@gmail.com>
2024-09-17 11:07:23 +0600

    feat : added numeric question file

---
+++ Number/Numeric_Question.java
@@ -0,0 +1,16 @@
+package Number;
+
+public class Numeric_Question {
+
+
+    public static boolean isUgly(int n) {
+
+        return true;
+        
+    }
+
+    public static void main(String[] args) {
+        
+    }
+    
+}
