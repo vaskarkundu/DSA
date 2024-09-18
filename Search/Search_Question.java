d917dd48f3d439bb7f64b0dd8f2fa2dba2c2b1b1
Vaskar Kundu <vaskarkundu18@gmail.com>
2024-09-18 16:14:01 +0600

    solution : findMin

--- Search/Search_Question.java
+++ Search/Search_Question.java
@@ -1,5 +1,7 @@
 package Search;
 
+import java.util.Arrays;
+
 public class Search_Question {
     
     public static int binarySearch(int[] arr, int ter){
@@ -11,11 +13,15 @@ public class Search_Question {
             int mid = low + (high - low) /2;
 
             if(arr[mid] == ter){
+                System.out.println("first"+arr[mid]);
                 return mid;
             }else if(arr[mid] < ter){
+                System.out.println("low"+arr[mid]);
                 low = mid + 1;
             }else{
+                System.out.println("high"+arr[mid]);
                  high = mid- 1;
+
             }
             
         }
@@ -23,9 +29,28 @@ public class Search_Question {
         return -1;
     }
 
+     public static int findMin(int[] nums) {
+
+        int left = 0;
+        int right = nums.length - 1;
+        while (left < right) {
+            
+            int mid = left + (right - left)/2;
+            
+            if(nums[mid] > nums[right]){
+                left = mid + 1;
+            }else{
+                right = mid;
+            }  
+            
+        }
+        return nums[left];
+        
+    }
+
     public static void main(String[] args) {
-        int[] nums = {1,2,3,4,5,6};
-        int x = binarySearch(nums, 4);
+        int[] nums = {11,13,15,17};
+        int x = findMin(nums);
         System.out.println(x);
     }
     
