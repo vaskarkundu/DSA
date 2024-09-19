d1d2ceab27e6a2cd9fc78309c35e36d07e18597b
Vaskar Kundu <vaskarkundu18@gmail.com>
2024-09-19 15:40:28 +0600

    solution : chekIfExist

--- Number/Numeric_Question.java
+++ Number/Numeric_Question.java
@@ -248,19 +248,32 @@ public class Numeric_Question {
 
         return res;
     }
+
+    public static boolean checkIfExist(int[] arr) {
+
+        for(int i = 0; i<arr.length; i++){
+            for(int j = 0; j<arr.length; j++){
+               
+                if (i != j) {
+                    if (arr[i] == 2 * arr[j] || arr[j] == 2 * arr[i]) {
+                        return true;
+                    }
+                }
+            }
+        }
+
+        return false;
+        
+    }
    
 
     public static void main(String[] args) {
-        int[] n1 ={1,2,2,1};
-        int[] n2 ={2,2};
+        int[] n1 ={7,1,14,11};
+       
      
-        int[] x = intersect(n1,n2);
-        for(int s : x){
-            System.out.println(s);
-        }
-
-        // int x = sum(12,14);
-        // System.out.println(x);
+        boolean x = checkIfExist(n1);
+       
+        System.out.println(x);
         
     }
     
