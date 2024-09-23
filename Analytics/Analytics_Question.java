47f3fe4e977ee6e0fe3e49fa74b60abeffe8f293
Vaskar Kundu <vaskarkundu18@gmail.com>
2024-09-23 16:34:07 +0600

    solution : distinct averages

--- Analytics/Analytics_Question.java
+++ Analytics/Analytics_Question.java
@@ -1,5 +1,10 @@
 package Analytics;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.HashSet;
+
 public class Analytics_Question {
 
     public static int numberOfCuts(int n) {
@@ -12,12 +17,32 @@ public class Analytics_Question {
         
     }
 
-    
+    public static int distinctAverages(int[] nums) {
+
+        Arrays.sort(nums);
+        HashSet<Double> li = new HashSet<>();
+       
+        int left = 0;
+        int right = nums.length - 1;
+        while (left < right) {
+            double avg = (nums[left] + nums[right]) / 2.0;
+           li.add(avg);
+           left++;
+           right--;
+            
+        }
+        return li.size();
+    }
+
+
     
 
     public static void main(String[] args) {
+        int[] n= {9,5,7,8,7,9,8,2,0,7};
+        int d = distinctAverages(n);
+        System.out.println(d);
 
-        numberOfCuts(3);
+        // numberOfCuts(3);
         
     }
 }
