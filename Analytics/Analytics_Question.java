968b73ea1a8df0471079a809d0c220055ad62dd5
Vaskar Kundu <vaskarkundu18@gmail.com>
2024-09-23 16:21:01 +0600

    solution : number of cuts

---
+++ Analytics/Analytics_Question.java
@@ -0,0 +1,23 @@
+package Analytics;
+
+public class Analytics_Question {
+
+    public static int numberOfCuts(int n) {
+
+        int x = 1 + (n*(n+1))/2;
+
+        System.out.println(x);
+
+        return n;
+        
+    }
+
+    
+    
+
+    public static void main(String[] args) {
+
+        numberOfCuts(3);
+        
+    }
+}
