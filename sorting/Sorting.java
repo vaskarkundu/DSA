76be8f059c0f1f6c385b236a67b5f650024599be
Vaskar Kundu <vaskarkundu18@gmail.com>
2024-09-25 12:29:14 +0600

    cleanup

--- sorting/Sorting.java
+++ sorting/Sorting.java
@@ -230,12 +230,7 @@ public class Sorting {
             }
         }
 
-        return maxLength;
-       
-    
-
-        
-        
+        return maxLength;   
     }
     public static void main(String[] args) {
         int [] nums = {100,4,200,1,3,2};
