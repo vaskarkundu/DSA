2313b63490bdd52588a12fc3afe217aa02817fd2
Vaskar Kundu <vaskarkundu18@gmail.com>
2024-10-17 16:09:26 +0600

    solution : backspace stack string

--- Stack/Stack_Question.java
+++ Stack/Stack_Question.java
@@ -31,22 +31,99 @@ public class Stack_Question {
 
     }
 
+   
+
+    public static void backspace(Stack<Character> s, Stack<Character> t) {
+        if (s.isEmpty() && t.isEmpty()) {
+            return; 
+        }
+        if (!s.isEmpty()) {
+            char sc = s.peek();
+            if (sc == '#') {
+             
+                s.pop(); 
+                if(!s.isEmpty()){
+
+                    s.pop();
+                }
+                backspace(s, t);
+            }
+        }
+    
+       
+        if (!t.isEmpty()) {
+            char tc = t.peek();
+            if (tc == '#') {
+                t.pop();
+                if(!t.isEmpty()){
+                    t.pop();
+                }
+                
+                backspace(s, t); 
+            }
+        }
+    
+       
+        if (!s.isEmpty() && !t.isEmpty()) {
+            
+            char sx = s.pop();
+            char tx = t.pop();
+          
+            backspace(s, t);
+           
+            s.push(sx);
+            t.push(tx);
+        }
+    }
+
+   
+    
+    public static boolean backspaceCompare(String s, String t) {
+
+        Stack<Character> sStack = back(s);
+        Stack<Character> tStack = back(t);
+
+        return sStack.equals(tStack);
+        
+    }
+
+    public static Stack<Character> back(String str){
+
+        Stack<Character> s = new Stack<>();
+
+        for(char ch : str.toCharArray()){
+            if(ch == '#'){
+                if(!s.isEmpty()){
+                    s.pop();
+                }
+            }else{
+                s.push(ch);
+            }
+        }
+
+        return s;
+
+    }
+
 
 
     public static void main(String[] args) {
+     
+       boolean x = backspaceCompare("ab##", "c#d#");
+       System.out.println(x);
 
-        Stack<Integer> s = new Stack<>();
-        s.push(1);
-        s.push(5);
-        s.push(10);
-        s.push(15);
-        addVlaueToBottom(s, 8);
-        reverseStack(s);
-        while (!s.isEmpty()) {
-            System.out.println(s.peek());
-            s.pop();
+        // Stack<Integer> s = new Stack<>();
+        // s.push(1);
+        // s.push(5);
+        // s.push(10);
+        // s.push(15);
+        // addVlaueToBottom(s, 8);
+        // reverseStack(s);
+        // while (!s.isEmpty()) {
+        //     System.out.println(s.peek());
+        //     s.pop();
             
-        }
+        // }
         
     }
     
