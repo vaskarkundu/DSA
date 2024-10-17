ec513efcdfc54e775e89465ee8cc65604bc058c3
Vaskar Kundu <vaskarkundu18@gmail.com>
2024-10-17 11:22:14 +0600

    solution : added value to bottom in stack

--- Stack/Stack_Question.java
+++ Stack/Stack_Question.java
@@ -1,9 +1,38 @@
 package Stack;
 
+import java.util.*;
+
 public class Stack_Question {
+    
+
+    // solution  add value to bottom
+    public static void addVlaueToBottom(Stack<Integer> s,int val){
+        if(s.isEmpty()){
+            s.push(val);
+            return;
+
+        }
+        int tem = s.pop();
+        addVlaueToBottom(s, val);
+        s.push(tem);
+
+    }
+
 
 
     public static void main(String[] args) {
+
+        Stack<Integer> s = new Stack<>();
+        s.push(1);
+        s.push(5);
+        s.push(10);
+        s.push(15);
+        addVlaueToBottom(s, 8);
+        while (!s.isEmpty()) {
+            System.out.println(s.peek());
+            s.pop();
+            
+        }
         
     }
     
