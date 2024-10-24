fc0209d45ed2f3acd1e1144f0c8db5133cacfc4e
Vaskar Kundu <vaskarkundu18@gmail.com>
2024-10-24 11:38:24 +0600

    solution : sliding window approach

---
+++ Approach/Diff_Approach.java
@@ -0,0 +1,28 @@
+package Approach;
+
+public class Diff_Approach {
+
+    // Sliding Window Approach
+    public static int maximumSubarray(int arr[], int k){
+        if(arr.length < k) return -1;
+        int max = 0;
+        for(int i =0; i<k; i++){
+            max += arr[i];
+
+        }
+        int windowSLiding = max;
+        for(int i =k; i<arr.length; i++){
+            windowSLiding = arr[i] - arr[i -k];
+        }
+        
+
+        return Math.max(max, windowSLiding);
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {1,2,3,4,5,6,7,8};
+        int x = maximumSubarray(arr, 3);
+        
+    }
+    
+}
