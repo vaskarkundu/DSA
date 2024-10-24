b8296be361d2116aacca0ac8af251f3c789da479
Vaskar Kundu <vaskarkundu18@gmail.com>
2024-10-24 17:25:40 +0600

    cleanup

--- Stack/Stack_Question.java
+++ Stack/Stack_Question.java
@@ -421,8 +421,25 @@ public class Stack_Question {
         }
         return 0;
     }
+//     Input: nums = [1,2,1]
+// Output: [2,-1,2]
+// Explanation: The first 1's next greater number is 2; 
+// The number 2 can't find next greater number. 
+// The second 1's next greater number needs to search circularly, which is also 2.
+// Example 2:
 
+// Input: nums = [1,2,3,4,3]
+// Output: [2,3,4,-1,4]
+   
     public static int[] nextGreaterElements(int[] nums) {
+
+        Stack<Integer> stack = new Stack<>();
+        ArrayList<Integer> ar = new ArrayList<>();
+
+
+    
+        
+        
         return nums;
     }
 
